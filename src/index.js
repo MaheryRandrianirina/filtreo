81be90c162716cbd78d0ab2db96c0aa82c52f054
MaheryRandrianirina <106425085+MaheryRandrianirina@users.noreply.github.com>
2025-01-16 20:30:31 +0300

    feat: add style

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { HTMLTable } from "./HTMLTable.js";
 
 const filterButton = document.querySelector(".filter-button");
 if(filterButton) {
-    console.log(document.querySelector("table"))
     const table = new HTMLTable(document.querySelector("table"), {
         doubleUniqueValues: {5: ["done", "to do"]}
     });
